3a9efa6d971da0e29c3fe6e934b62bf87796578d
Cedric Lecocq <clecocq@zepto-technology.fr>
2024-09-09 14:42:37 +0200

    Les formulaires

--- src/app/velib/recherche/recherche.component.ts
+++ src/app/velib/recherche/recherche.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-recherche',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './recherche.component.html',
   styleUrl: './recherche.component.css'
 })
 export class RechercheComponent {
-
+  rechercheForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    isInstalled: new FormControl(false),
+    isRenting: new FormControl(false),
+    idReturning: new FormControl(false)
+  })
+  protected readonly JSON = JSON;
 }
