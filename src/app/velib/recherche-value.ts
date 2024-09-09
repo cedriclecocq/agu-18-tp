e26c30937560c0e7da1f6dd89d59dd18a548659d
Cedric Lecocq <clecocq@zepto-technology.fr>
2024-09-09 15:38:39 +0200

    Les hots observables

---
+++ src/app/velib/recherche-value.ts
@@ -0,0 +1,6 @@
+export interface RechercheValue {
+  name: string | null,
+  isInstalled: boolean | null,
+  isRenting: boolean | null,
+  idReturning: boolean | null
+}
--- src/app/velib/recherche/recherche.component.ts
+++ src/app/velib/recherche/recherche.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { debounceTime, filter } from "rxjs";
+import { RechercheValue } from "../recherche-value";
 
 @Component({
   selector: 'app-recherche',
@@ -21,12 +22,17 @@ export class RechercheComponent {
 
   protected readonly JSON = JSON;
 
+  values: EventEmitter<Partial<RechercheValue>> = new EventEmitter<Partial<RechercheValue>>();
+
   constructor() {
     this.rechercheForm.valueChanges.pipe(
       filter(formValue => formValue.name?.length ? formValue.name.length >= 3 : false),
       debounceTime(500)
     ).subscribe({
-      next: value => console.log(value)
+      next: value => this.values.emit(value)
     });
+
+    this.values.subscribe(value => console.log(`obs1 : ${value.name}`));
+    this.values.subscribe(value => console.log(`obs2 : ${value.name}`));
   }
 }
